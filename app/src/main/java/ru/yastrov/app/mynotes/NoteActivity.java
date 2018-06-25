013d4beff14f7f3e6bd6a8f7f5078afaca1958f8
Yuri Astrov <yuriastrov@gmail.com>
2018-06-25 23:34:47 +0300

    NoteActivity: prepare for create with Intent.

--- app/src/main/java/ru/yastrov/app/mynotes/NoteActivity.java
+++ app/src/main/java/ru/yastrov/app/mynotes/NoteActivity.java
@@ -1,13 +1,23 @@
 package ru.yastrov.app.mynotes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+
 
 public class NoteActivity extends AppCompatActivity {
+
+    private EditText editTitle,
+                     editContent;
+    private String fileName;
+    public static final String NOTE_ACTION_OPEN = "ru.yastrov.app.mynotes.action.note_open";
+    public static final String NOTE_ACTION_CREATE = "ru.yastrov.app.mynotes.action.note_create";
+    public static final String INTENT_EXTRA_FILENAME = "fname";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +30,21 @@ public class NoteActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        editTitle = (EditText) findViewById(R.id.editNoteTitle);
+        editContent = (EditText) findViewById(R.id.editNoteContent);
+
+        final Intent intent = getIntent();
+        if(intent.getAction() == NOTE_ACTION_OPEN) {
+            fileName = intent.getStringExtra(INTENT_EXTRA_FILENAME);
+        }
+        if (intent.getAction() == NOTE_ACTION_CREATE) {
+        }
     }
 
 }
