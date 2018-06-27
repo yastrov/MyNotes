5e9ad7f89ec65b2ec399e1f867f0007d33d4303b
Yuri Astrov <yuriastrov@gmail.com>
2018-06-27 22:54:57 +0300

    NoteListActivity: RecyclerView has been added.

--- app/src/main/java/ru/yastrov/app/mynotes/NoteListActivity.java
+++ app/src/main/java/ru/yastrov/app/mynotes/NoteListActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -13,6 +16,7 @@ import android.widget.ProgressBar;
 
 public class NoteListActivity extends AppCompatActivity {
     private ProgressBar progressBar;
+    private RecyclerView notesRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,13 @@ public class NoteListActivity extends AppCompatActivity {
             }
         });
         progressBar = (ProgressBar) findViewById(R.id.noteListProgressBar);
+
+        notesRecycler = (RecyclerView) findViewById(R.id.notes);
+        notesRecycler.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager =
+                new LinearLayoutManager(getApplicationContext()); /*Or LinearLayoutManager(this)*/
+        notesRecycler.setLayoutManager(mLayoutManager);
+        notesRecycler.setItemAnimator(new DefaultItemAnimator());
     }
 
     @Override
