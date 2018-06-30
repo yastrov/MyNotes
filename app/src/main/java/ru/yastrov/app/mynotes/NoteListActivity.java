599eb56705fd0d0368df32a68f2e7a6f19dbfbf4
Yuri Astrov <yuriastrov@gmail.com>
2018-06-30 12:11:09 +0300

    Log has been added.

--- app/src/main/java/ru/yastrov/app/mynotes/NoteListActivity.java
+++ app/src/main/java/ru/yastrov/app/mynotes/NoteListActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,6 +26,7 @@ import java.util.Date;
 import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
+    private static final String TAG = "NoteListActivity";
     private ProgressBar progressBar;
     private RecyclerView notesRecycler;
     private List<NoteItem> notesList = new ArrayList<>();
@@ -48,12 +50,14 @@ public class NoteListActivity extends AppCompatActivity {
         });
         progressBar = (ProgressBar) findViewById(R.id.noteListProgressBar);
 
+        Log.d(TAG, "onCreate notesRecycler config");
         notesRecycler = (RecyclerView) findViewById(R.id.notes);
         notesRecycler.setHasFixedSize(true);
         RecyclerView.LayoutManager mLayoutManager =
                 new LinearLayoutManager(getApplicationContext()); /*Or LinearLayoutManager(this)*/
         notesRecycler.setLayoutManager(mLayoutManager);
         notesRecycler.setItemAnimator(new DefaultItemAnimator());
+        Log.d(TAG, "onCreate ReadNoteListTask().execute()");
         new ReadNoteListTask().execute();
     }
 
@@ -90,6 +94,7 @@ public class NoteListActivity extends AppCompatActivity {
     }
 
     public boolean fileExists(String fname) {
+        Log.d(TAG, "onCreate ReadNoteListTask().execute()");
         final File file = getBaseContext().getFileStreamPath(fname);
         return file.exists();
     }
@@ -98,19 +103,23 @@ public class NoteListActivity extends AppCompatActivity {
 
         @Override
         protected List<NoteItem> doInBackground(Void... files) {
+            Log.d(TAG, "ReadNoteListTask doInBackground");
             return prepareNotes();
         }
 
         @Override
         protected void onPreExecute() {
+            Log.d(TAG, "ReadNoteListTask onPreExecute");
             showTaskWorked();
         }
 
         @Override
         protected void onPostExecute(List<NoteItem> result) {
+            Log.d(TAG, "ReadNoteListTask onPostExecute onPostExecute");
             showTaskStoped();
             notesList = result;
             if(result != null ) {
+                Log.d(TAG, "ReadNoteListTask onPostExecute new NotesAdapter");
                 nAdapter = new NotesAdapter(result, new RecyclerViewClickListener() {
                     @Override
                     public void onClick(View v, int position) {
@@ -121,14 +130,17 @@ public class NoteListActivity extends AppCompatActivity {
                         startActivity(intent);
                     }
                 });
+                Log.d(TAG, "ReadNoteListTask onPostExecute onPostExecute notesRecycler.setAdapter");
                 notesRecycler.setAdapter(nAdapter);
             } else {
+                Log.d(TAG, "ReadNoteListTask onPostExecute Snackbar: " + getResources().getString(R.string.error_cant_load_file));
                 Snackbar.make(findViewById(android.R.id.content), getResources().getString(R.string.error_cant_load_file), Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
             }
         }
 
         private List<NoteItem> prepareNotes() {
+            Log.d(TAG, "ReadNoteListTask prepareNotes");
             List<NoteItem> notes = new ArrayList<>();
             File directory = getFilesDir();
             File[] files = directory.listFiles();
@@ -136,6 +148,7 @@ public class NoteListActivity extends AppCompatActivity {
                 NoteItem note = openFile(files[i].getPath());
                 notes.add(note);
             }
+            Log.d(TAG, "ReadNoteListTask prepareNotes notes.sort");
             //notes.sort(Comparator.comparing(NoteItem::getDate));
             notes.sort(new Comparator<NoteItem>() {
                 @Override
@@ -147,6 +160,7 @@ public class NoteListActivity extends AppCompatActivity {
         }
 
         private NoteItem openFile(String fileName) {
+            Log.d(TAG, "ReadNoteListTask openFile: " + fileName);
             NoteItem result = new NoteItem();
             result.setFileName(fileName);
             if (fileExists(fileName)) {
@@ -161,11 +175,13 @@ public class NoteListActivity extends AppCompatActivity {
                             result.setTitle(str);
                         }
                         str = reader.readLine();
+                        Log.d(TAG, "openFile parse date: " + str);
                         final Date date = DateHelper.parseStringToDate(str);
+                        Log.d(TAG, "openFile date parsed: " + date.toString());
                         result.setDate(date);
                     }
                 } catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
-
+                    Log.e(TAG, "ReadNoteListTask saveFile EXCEPTION: " + t.toString(), t);
                 }
             }
             return result;
